1f62f7b85c26abff65fa7f085c1aa05f8be334f1
Julian Dicesaro <juliannahueldicesaro@gmail.com>
2024-07-30 10:11:36 -0300

    Move serveraddress and port to the main (client)

--- src/Client.java
+++ src/Client.java
@@ -4,8 +4,12 @@ import client.ClientTCP;
 
 public class Client {
 
+    // Moverlo a un archivo de configuracion no versionado
+    private static final String SERVER_ADDRESS = "localhost"; // Cambia a la IP del servidor si no está en la misma máquina
+    private static final int TCP_PORT = 12345;
+
     public static void main(String[] args) {
-        Runnable clientTCP = new ClientTCP();
+        Runnable clientTCP = new ClientTCP(SERVER_ADDRESS, TCP_PORT);
         new Thread(clientTCP).start();
     }
 }
--- src/client/ClientTCP.java
+++ src/client/ClientTCP.java
@@ -4,15 +4,20 @@ import java.net.*;
 
 public class ClientTCP implements Runnable {
 
-    private static final String SERVER_ADDRESS = "localhost"; // Cambia a la IP del servidor si no está en la misma máquina
-    private static final int TCP_PORT = 12345;
     private static int clientId;
     private static final String UDP_PORT_MESSAGE_PATTERN = "UDP:<udpPort>";
+    private String serverAddress;
+    private int tcpPort;
+
+    public ClientTCP(String serverAddress, int tcpPort) {
+        this.serverAddress = serverAddress;
+        this.tcpPort = tcpPort;
+    }
 
     @Override
     public void run() {
 
-        try (Socket socket = new Socket(SERVER_ADDRESS, TCP_PORT);
+        try (Socket socket = new Socket(serverAddress, tcpPort);
             BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in))) {
